1b1f70a346a89dea89992570eff71cf28898a356
dbc <dbcc@users.noreply.github.com>
2015-06-08 08:23:52 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
+	s "strings"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-//Response struct.
-type Response struct {
+type WSConn struct {
+	conn   *websocket.Conn
+	dialer websocket.Dialer
+	heads  http.Header
+}
+
+type Message struct {
 	Nick      string   `json:"nick"`
 	NickLower string   `json:"nicklower"`
 	Features  []string `json:"features"`
@@ -24,221 +25,104 @@ type Response struct {
 	Command   string   `json:"command"`
 }
 
-type UserData struct {
-	Nick    string
-	File    *os.File
-	LastMSG time.Time
-}
+var (
+	url       = "ws://destiny.gg:9998/ws"
+	authToken = []string{"authtoken=1234567890"}
+	origin    = []string{"http://destiny.gg"}
+)
 
-// WriteFile uses the returned struct from parse to log it into a file.
-func WriteFile(ch chan Response) {
-	var userdata []UserData
-
-	go func() {
-		for {
-			userdata = closeFile(userdata)
-			time.Sleep(10 * time.Second)
-		}
-	}()
-
-	for {
-		message := <-ch
-		filePath := "public/_public/Destinygg chatlog/"
-		messageTime := time.Unix(message.Timestamp/1000, 0).UTC()
-		currDate := messageTime.Format("01-02-2006")
-		currTime := messageTime.Format("[Jan 02 2006 15:04:05 MST] ")
-		monthYear := messageTime.Format("January 2006")
-
-		CheckFolders(monthYear)
-
-		if message.Command == "BROADCAST" {
-			f, err := openFile(fmt.Sprintf("%s%s/subs.txt", filePath, monthYear))
-			if err != nil {
-				log.Println(err)
-			}
-			f.WriteString(currTime + message.Data + "\n")
-			f.Close()
-		}
-		if message.Command == "BAN" {
-			f, err := openFile(fmt.Sprintf("%s%s/bans.txt", filePath, monthYear))
-			if err != nil {
-				log.Println(err)
-			}
-			f.WriteString(currTime + message.Data + " banned by " + message.Nick + "\n")
-			f.Close()
-		}
-		if message.Command == "UNBAN" {
-			f, err := openFile(fmt.Sprintf("%s%s/bans.txt", filePath, monthYear))
-			if err != nil {
-				log.Println(err)
-			}
-			f.WriteString(currTime + message.Data + " unbanned by " + message.Nick + "\n")
-			f.Close()
-		}
-		if message.Command == "MUTE" {
-			f, err := openFile(fmt.Sprintf("%s%s/bans.txt", filePath, monthYear))
-			if err != nil {
-				log.Println(err)
-			}
-			f.WriteString(currTime + message.Data + " muted by " + message.Nick + "\n")
-			f.Close()
-		}
-		if message.Command == "UNMUTE" {
-			f, err := openFile(fmt.Sprintf("%s%s/bans.txt", filePath, monthYear))
-			if err != nil {
-				log.Println(err)
-			}
-			f.WriteString(currTime + message.Data + " unmuted by " + message.Nick + "\n")
-			f.Close()
-		}
-		if message.Command == "MSG" {
-			f, err := openFile(fmt.Sprintf("%s%s/%s.txt", filePath, monthYear, currDate))
-			if err != nil {
-				log.Println("Error writing mainlog", err)
-			}
-			f.WriteString(currTime + message.Nick + ": " + message.Data + "\n")
-
-			err = message.write(userdata, currTime)
-			if err != nil {
-				f, err := openFile(fmt.Sprintf("%s%s/userlogs/%s.txt", filePath, monthYear, message.Nick))
-				if err != nil {
-					log.Println("Error writing userlog", err)
-				}
-				f.WriteString(currTime + message.Nick + ": " + message.Data + "\n")
-				userdata = append(userdata, UserData{message.Nick, f, time.Now()})
-			}
-
-		}
+func (w *WSConn) Reconnect() {
+	if w.conn != nil {
+		_ = w.conn.Close()
 	}
-}
 
-func openFile(p string) (*os.File, error) {
-	f, err := os.OpenFile(fmt.Sprintf(p),
-		os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	var err error
+	w.conn, _, err = w.dialer.Dial(url, w.heads)
 	if err != nil {
-		return nil, err
+		logAndDelay(err)
+		w.Reconnect()
+		return
 	}
-	return f, nil
 }
 
-func closeFile(ud []UserData) []UserData {
-	if len(ud) > 0 {
-		// log.Println("closing files startet-------------------------")
-		for i, d := range ud {
-			diff := time.Since(d.LastMSG)
-			if diff.Seconds() > 59.0 {
-				d.File.Close()
-				// log.Println(d.Nick, "closed")
-				ud = append(ud[:i], ud[i+1:]...)
-			}
-		}
-	}
-	return ud
-}
-
-func (r Response) write(ch []UserData, t string) error {
-	for _, res := range ch {
-		if res.Nick == r.Nick {
-			res.File.WriteString(t + r.Nick + ": " + r.Data + "\n")
-			res.update()
-			return nil
-		}
-	}
-	return errors.New("nope")
-}
-
-// Parse the raw message and return the struct.
-func Parse(m []byte, r Response) Response {
-	s := strings.SplitAfterN(string(m), " ", 2)
-	if len(s) != 2 {
-		return Response{}
-	}
-	err := json.Unmarshal([]byte(s[1]), &r)
+func (w *WSConn) Read() Message {
+	err := w.conn.SetReadDeadline(time.Now().Add(20 * time.Second))
 	if err != nil {
-		return Response{}
+		w.Reconnect()
+		w.Read()
 	}
-	r.Command = strings.Trim(s[0], " ")
-	r.Data = strings.Replace(r.Data, "\n", "", -1)
-	r.NickLower = strings.ToLower(r.Nick)
-	return r
+
+	_, msg, err := w.conn.ReadMessage()
+	if err != nil {
+		logAndDelay(err)
+		w.Reconnect()
+
+	}
+	return parse(msg)
 }
 
-// CheckFolders checks if the folders exist if not -> create them.
-func CheckFolders(d string) {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+func (w *WSConn) Write(msgType, msg string) {
+	err := w.conn.WriteMessage(1, []byte(msgType+` {"data":"`+msg+`"}`))
 	if err != nil {
-		log.Println(err)
-	}
-	p := filepath.Join(dir, "public/_public/Destinygg chatlog/"+d+"/userlogs")
-	_, err = os.Stat(p)
-	if err != nil {
-		err = os.MkdirAll(p, 0755)
-		if err != nil {
-			log.Println(err)
-		}
+		logAndDelay(err)
+		w.Reconnect()
 	}
 }
 
-func (u UserData) update() {
-	u.LastMSG = time.Now()
+func (w *WSConn) WritePrivate(u string, msg string) {
+	w.conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	err := w.conn.WriteMessage(1, []byte(`PRIVMSG {"nick":"`+u+`", data":"`+msg+`"}`))
+	if err != nil {
+		logAndDelay(err)
+		w.Reconnect()
+	}
+}
+
+func parse(msg []byte) (m Message) {
+	list := s.SplitAfterN(string(msg), " ", 2)
+	if len(list) != 2 {
+		return Message{}
+	}
+
+	err := json.Unmarshal([]byte(list[1]), &m)
+	if err != nil {
+		return Message{}
+	}
+
+	m.Command = s.Trim(list[0], " ")
+	m.Data = s.Replace(m.Data, "\n", "", -1)
+	m.NickLower = s.ToLower(m.Nick)
+	return m
+}
+
+func logAndDelay(err error) {
+	log.Printf("Connection failed ERROR: %s\n", err)
+	log.Println("Reconnecting in 5 Seconds...")
+	time.Sleep(5 * time.Second)
 }
 
 func main() {
-	var (
-		msg     = make([]byte, 1024)
-		resChan = make(chan Response, 10)
-		addr    = "ws://destiny.gg:9998/ws"
-		dialer  = websocket.Dialer{HandshakeTimeout: 5 * time.Second}
-		headers = http.Header{"Origin": []string{"http://destiny.gg/"}}
-	)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	var chLog = make(chan Message, 10)
+	var chHandle = make(chan Message, 2)
 
-	go WriteFile(resChan)
+	go WriteFile(chLog)
 
-	log.Println("Connecting to", addr)
-again:
-	ws, _, err := dialer.Dial(addr, headers)
-	if err != nil {
-		log.Println("Reonnecting in 5 seconds...")
-		time.Sleep(5 * time.Second)
-		goto again
+	ws := &WSConn{
+		dialer: websocket.Dialer{HandshakeTimeout: 20 * time.Second},
+		heads:  http.Header{"Origin": origin, "Cookie": authToken},
 	}
-	defer ws.Close()
+	log.Println("Connecting to", url)
+	ws.Reconnect()
+	log.Println("Connected to", url)
 
-	log.Println("Connection Successful!")
+	go HandleMessage(ws, chHandle)
+
 	for {
-		ws.SetReadDeadline(time.Now().Add(20 * time.Second))
-		_, msg, err = ws.ReadMessage()
-		if err != nil {
-			ws.Close()
-			log.Printf("Reconnecting to server in %d seconds\n", 5)
-			goto again
-		}
+		msg := ws.Read()
+		// log.Println(msg)
 
-		// log.Println(string(msg))
-
-		res := Response{}
-		res = Parse(msg, res)
-		if res.Command == "BAN" || res.Command == "UNBAN" {
-			log.Printf("%s > %s un/banned %s", res.Command, res.Nick, res.Data)
-			resChan <- res
-		}
-		if res.Command == "MUTE" || res.Command == "UNMUTE" {
-			log.Printf("%s > %s un/muted %s", res.Command, res.Nick, res.Data)
-			resChan <- res
-		}
-		if res.Command == "MSG" {
-			log.Printf("%s > %s: %s", res.Command, res.Nick, res.Data)
-			resChan <- res
-		}
-		if res.Command == "BROADCAST" {
-			if strings.Contains(res.Data, "subscriber!") {
-				log.Printf("%s > %s", res.Command, res.Data)
-				resChan <- res
-			}
-			if strings.Contains(res.Data, "resubscribed on Twitch!") {
-				log.Printf("%s > %s", res.Command, res.Data)
-				resChan <- res
-			}
-		}
+		chLog <- msg
+		chHandle <- msg
 	}
 }
