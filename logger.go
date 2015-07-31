848bb3606bc1f9feb33e293e15dab18b14a238c2
dbc <dbcc@users.noreply.github.com>
2015-08-01 01:44:39 +0200

    Update logger.go

--- logger.go
+++ logger.go
@@ -14,74 +14,77 @@ func WriteFile(ch <-chan Message) {
 
 	for {
 		res := <-ch
-		filePath := "public/_public/Destinygg chatlog/"
+		basePath := "public/_public/Destinygg chatlog/"
 		timestamp := time.Unix(res.Timestamp/1000, 0).UTC()
-		currDate := timestamp.Format("01-02-2006")
-		currTime := timestamp.Format("[Jan 02 2006 15:04:05 MST] ")
+		msgDate := timestamp.Format("2006-01-02")
+		msgTime := timestamp.Format("[2006-01-02 15:04:05 MST] ")
 		monthYear := timestamp.Format("January 2006")
+		currMonthYear := time.Now().Format("January 2006")
 
-		checkFolders(monthYear)
-		go writePremiumFile(res.Nick, res.Data, currTime, monthYear)
+		checkFolders(basePath + currMonthYear + "/userlogs/")
+		go writePremiumFile(res.Nick, res.Data, msgTime, currMonthYear)
 
 		switch res.Command {
-		case "BROADCAST":
-			f, err := openFile(fmt.Sprintf("%s%s/subs.txt", filePath, monthYear))
-			if err != nil {
-				LogErr(err)
-				continue
-			}
-			f.WriteString(currTime + res.Data + "\n")
-			f.Close()
 		case "BAN":
-			f, err := openFile(fmt.Sprintf("%s%s/bans.txt", filePath, monthYear))
+			f, err := OpenFile(fmt.Sprintf("%s%s/bans.txt", basePath, monthYear))
 			if err != nil {
 				LogErr(err)
 				continue
 			}
-			f.WriteString(currTime + res.Data + " banned by " + res.Nick + "\n")
+			f.WriteString(msgTime + res.Data + " banned by " + res.Nick + "\n")
 			f.Close()
 			log.Printf("%s > %s banned %s", res.Command, res.Nick, res.Data)
 		case "UNBAN":
-			f, err := openFile(fmt.Sprintf("%s%s/bans.txt", filePath, monthYear))
+			f, err := OpenFile(fmt.Sprintf("%s%s/bans.txt", basePath, monthYear))
 			if err != nil {
 				LogErr(err)
 				continue
 			}
-			f.WriteString(currTime + res.Data + " unbanned by " + res.Nick + "\n")
+			f.WriteString(msgTime + res.Data + " unbanned by " + res.Nick + "\n")
 			f.Close()
 			log.Printf("%s > %s unbanned %s", res.Command, res.Nick, res.Data)
 		case "MUTE":
-			f, err := openFile(fmt.Sprintf("%s%s/bans.txt", filePath, monthYear))
+			f, err := OpenFile(fmt.Sprintf("%s%s/bans.txt", basePath, monthYear))
 			if err != nil {
 				LogErr(err)
 				continue
 			}
-			f.WriteString(currTime + res.Data + " muted by " + res.Nick + "\n")
+			f.WriteString(msgTime + res.Data + " muted by " + res.Nick + "\n")
 			f.Close()
 			log.Printf("%s > %s muted %s", res.Command, res.Nick, res.Data)
 		case "UNMUTE":
-			f, err := openFile(fmt.Sprintf("%s%s/bans.txt", filePath, monthYear))
+			f, err := OpenFile(fmt.Sprintf("%s%s/bans.txt", basePath, monthYear))
 			if err != nil {
 				LogErr(err)
 				continue
 			}
-			f.WriteString(currTime + res.Data + " unmuted by " + res.Nick + "\n")
+			f.WriteString(msgTime + res.Data + " unmuted by " + res.Nick + "\n")
 			f.Close()
 			log.Printf("%s > %s unmuted %s", res.Command, res.Nick, res.Data)
+		case "BROADCAST":
+			if s.Contains(res.Data, "subscriber!") || s.Contains(res.Data, "resubscribed on Twitch!") {
+				f, err := OpenFile(fmt.Sprintf("%s%s/subs.txt", basePath, monthYear))
+				if err != nil {
+					LogErr(err)
+					continue
+				}
+				f.WriteString(msgTime + res.Data + "\n")
+				f.Close()
+			}
 		case "MSG":
-			f, err := openFile(fmt.Sprintf("%s%s/%s.txt", filePath, monthYear, currDate))
+			f, err := OpenFile(fmt.Sprintf("%s%s/%s.txt", basePath, monthYear, msgDate))
 			if err != nil {
 				LogErr(err)
 				continue
 			}
-			f.WriteString(currTime + res.Nick + ": " + res.Data + "\n")
+			f.WriteString(msgTime + res.Nick + ": " + res.Data + "\n")
 			f.Close()
-			f, err = openFile(fmt.Sprintf("%s%s/userlogs/%s.txt", filePath, monthYear, res.Nick))
+			f, err = OpenFile(fmt.Sprintf("%s%s/userlogs/%s.txt", basePath, monthYear, res.Nick))
 			if err != nil {
 				LogErr(err)
 				continue
 			}
-			f.WriteString(currTime + res.Nick + ": " + res.Data + "\n")
+			f.WriteString(msgTime + res.Nick + ": " + res.Data + "\n")
 			f.Close()
 			log.Printf("%s > %s: %s", res.Command, res.Nick, res.Data)
 		}
@@ -94,47 +97,33 @@ func LogErr(err error) {
 	}
 }
 
-func openFile(p string) (*os.File, error) {
-	f, err := os.OpenFile(fmt.Sprintf(p),
+func OpenFile(p string) (*os.File, error) {
+	return os.OpenFile(fmt.Sprintf(p),
 		os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
-	if err != nil {
-		LogErr(err)
-		return nil, err
-	}
-	return f, nil
 }
 
-func checkFolders(d string) {
+func checkFolders(p string) {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		LogErr(err)
 		return
 	}
 
-	p := filepath.Join(dir, "public/_public/Destinygg chatlog/"+d+"/userlogs/")
+	p = filepath.Join(dir, p)
 	_, err = os.Stat(p)
 	if err != nil {
-		createFolders("Destinygg chatlog/" + d + "/userlogs/")
+		createFolders(p)
 		return
 	}
 }
 
 func createFolders(p string) {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	err := os.MkdirAll(p, 0755)
 	if err != nil {
 		LogErr(err)
 		return
 	}
 
-	p = filepath.Join(dir, "public/_public/"+p)
-	_, err = os.Stat(p)
-	if err != nil {
-		err = os.MkdirAll(p, 0755)
-		if err != nil {
-			LogErr(err)
-			return
-		}
-	}
 }
 
 func writePremiumFile(nick, msg, time, date string) {
@@ -148,16 +137,16 @@ func writePremiumFile(nick, msg, time, date string) {
 
 	for _, n := range namelist {
 		if s.Contains(s.ToLower(msg), s.ToLower(n)) {
-			createFolders("premium/" + s.ToLower(n))
+			checkFolders("public/_public/premium/" + s.ToLower(n))
 
-			f, err := openFile("public/_public/premium/" + s.ToLower(n) + "/" + date + ".txt")
+			f, err := OpenFile("public/_public/premium/" + s.ToLower(n) + "/" + date + ".txt")
 			if err != nil {
 				LogErr(err)
 				return
 			}
+			defer f.Close()
 
 			f.WriteString(time + nick + ": " + msg + "\n")
-			f.Close()
 		}
 	}
 }
