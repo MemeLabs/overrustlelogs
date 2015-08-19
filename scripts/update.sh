1aafc752b5344d34793648f8359032bdf86308b0
dbc <dbc@overrustlelogs.net>
2015-08-19 04:13:51 +0200

    reformatted update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,24 +5,60 @@ export src="github.com/slugalisk/overrustlelogs"
 git pull
 
 source /etc/profile
-go install $src/logger
-go install $src/server
-go install $src/bot
-go install $src/tool
 
-stop orl-logger
-stop orl-server
-stop orl-bot
+if [[ $1 == "bot" ]]; then
+	echo "updating the orl-bot...\n"
+	updateBot
+elif [[ $1 == "server" ]]; then
+	echo "updating the orl-server...\n"
+	updateServer
+elif [[ $1 == "logger" ]]; then
+	echo "updating the orl-logger"
+	updateLogger
+else
+	echo "updating everything...\n"
+	updateBot
+	updateLogger
+	updateServer
+	echo "updating complete"
+fi
 
-cp -r $GOPATH/src/$src/server/views /var/overrustlelogs/
-cp -r $GOPATH/src/$src/server/assets /var/overrustlelogs/public/
-chown -R overrustlelogs:overrustlelogs /var/overrustlelogs/views
-chown -R overrustlelogs:overrustlelogs /var/overrustlelogs/public/assets
-cp $GOPATH/bin/logger /usr/bin/orl-logger
-cp $GOPATH/bin/server /usr/bin/orl-server
-cp $GOPATH/bin/bot /usr/bin/orl-bot
-cp $GOPATH/bin/tool /usr/bin/orl-tool
 
-start orl-logger
-start orl-server
-start orl-bot
+updateBot(){
+	go install $src/bot
+
+	stop orl-bot
+	
+	cp $GOPATH/bin/bot /usr/bin/orl-bot
+	
+	start orl-bot
+	echo "updated the orl-bot"
+}
+
+updateServer(){
+	go install $src/server
+	stop orl-server
+
+	cp -r $GOPATH/src/$src/server/views /var/overrustlelogs/
+	cp -r $GOPATH/src/$src/server/assets /var/overrustlelogs/public/
+	chown -R overrustlelogs:overrustlelogs /var/overrustlelogs/views
+	chown -R overrustlelogs:overrustlelogs /var/overrustlelogs/public/assets
+
+	cp $GOPATH/bin/server /usr/bin/orl-server
+
+	start orl-server
+	echo "updated the orl-server"
+}
+
+updateLogger(){
+	go install $src/logger
+	go install $src/tool
+
+	stop orl-logger
+
+	cp $GOPATH/bin/logger /usr/bin/orl-logger
+	cp $GOPATH/bin/tool /usr/bin/orl-tool
+
+	start orl-logger
+	echo "updated the orl-logger"
+}
